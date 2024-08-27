0a9b20fc0d25d352d337231f705c1e2c94fee7f7
franck gullit <frankiegullit@mail.com>
2024-08-27 11:10:39 +0200

    accueil scss (underlines on links undone)

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,19 +1,21 @@
 import './Card.scss';
 import '../../data/logements.json';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-function Card (props) {
-    const navigate = useNavigate();
+function Card(props) {
+  const navigate = useNavigate();
 
   const ImageClick = () => {
     navigate(`/Logements/${props.id}`);
   };
-    return(
-        <div className="cardInfo" onClick={ImageClick}>
-            <img src={props.cover} alt="cardImg" className="CardImage" />
-            <p className="CardTitle">{props.title}</p>
-        </div>
-    )
+  return (
+    <div className="cardInfo" onClick={ImageClick}>
+      <img src={props.cover} alt="cardImg" className="CardImage" />
+      <div className="CardTitle">
+        <p >{props.title}</p>
+      </div>
+    </div>
+  )
 }
 
 export default Card
