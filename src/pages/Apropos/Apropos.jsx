9a9d9daf13b54d6f0231b6724943a1f41c61103e
franck gullit <frankiegullit@mail.com>
2024-08-27 12:22:24 +0200

    apropos scss done(except underline)

--- src/pages/Apropos/Apropos.jsx
+++ src/pages/Apropos/Apropos.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import AproposBannerPic from '../../assets/DesktopAproposBanner.png';
 import Banner from '../../components/Banner/Banner.jsx';
 import Collapse from '../../components/Collapse/Collapse.jsx';
+import './Apropos.scss';
 
 
 function Apropos() {
